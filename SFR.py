fix bug in gradient removal

SFR.py
@@ -43,7 +43,7 @@ def centroid(arr, conv_kernel=3, win_width=5):
 
     win = np.zeros(arr.shape)
     for i in range(height):
-        win_c = np.argmax(np.convolve(arr[i, :], np.ones(conv_kernel), 'same'))
+        win_c = np.argmax(np.abs(np.convolve(arr[i, :], np.ones(conv_kernel), 'same')))
         win[i, win_c - win_width:win_c + win_width] = 1.0
 
         # x = np.ones((height, 1)) * np.arange(width)  # TODO shall we continue to use this?
@@ -466,7 +466,8 @@ def main():
     im = slanted_edge_target.make_slanted_curved_edge((N, N), angle=1 * 5.0,
                                                       curvature=-1 * 0.001 * 100 / N,
                                                       illum_gradient_magnitude=1 * +0.3,
-                                                      black_lvl=0.00)
+                                                      black_lvl=0.05)
+    im = im[:, ::-1]
     sample_edge = relative_luminance(im)
     for simulate_noise in [False]:  # [False, True]:
         # simulate photon noise

untitled4.py
@@ -78,16 +78,18 @@ def remove_gradient(image, idx_lo, idx_hi, dist=None, allowed_gradient=1e-4,
         # for side in ['lo', 'hi']:
         #         image_s[idx[side]] = image_s[idx[side]] - \
         #                          coefs[side][0] * xx[idx[side]] - coefs[side][1] * yy[idx[side]]
-                                 
+
+        # Flip sign of dist if the area left of the edge is the bright side
+        dist *= -1.0 if np.mean(dist[idx_lo]) > np.mean(dist[idx_hi]) else 1.0
+
         # Compensated (gradient removed) image:
-        
         esf = slanted_edge_target.InterpolateESF([-0.5, 0.5], [0.0, 1.0]).f
         f = {'lo': 1.0 + (0.0 - 1.0) * esf(dist),
-             'hi': 0.0 + (1.0 - 0.0) * esf(dist)}
+              'hi': 0.0 + (1.0 - 0.0) * esf(dist)}
         image_s = np.zeros(image.shape)
         for side in ['lo', 'hi']:
             image_s += f[side] * (image - coefs[side][0] * xx - coefs[side][1] * yy)
-                                 
+
     else:
         image_s = image * 1.0  # TODO: copy image in a nicer way
         bl = None