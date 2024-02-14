140079471c7fc53e3b5326627549b653180f26e0
Carl Asplund <carl.asplund@eclipseoptics.com>
2024-02-14 17:44:33 +0100

    utils.py: add functionality regarding MTF analysis and plotting

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
+import copy
 
 
 class Raw:
@@ -19,15 +21,186 @@ class Raw:
             f.write(output)
 
 
-# def read_raw(path, height=2200, width=3200, rshift=4, fmt='<u2'):
-#     image = np.fromfile(path, dtype=np.dtype(fmt)) >> rshift
-#     return np.reshape(image, (height, width)).astype(float)
-#
-#
-# def write_raw(im_data, path, lshift=4, fmt='<u2'):
-#     output = (im_data.flatten().astype(fmt) << lshift).tobytes()
-#     with open(path, 'wb') as f:
-#         f.write(output)
+def airy_disk(lam_diffr, f_number, grid_spacing, psf_shape):
+    y_max, x_max = psf_shape
+    xx, yy = np.meshgrid(np.arange(y_max), np.arange(x_max))
+    r = np.sqrt((xx - 0.5 * x_max) ** 2 + (yy - 0.5 * y_max) ** 2) * grid_spacing
+    r_prime = np.pi * r / (lam_diffr * f_number)
+    r_prime = np.where(r_prime == 0, 1e-16, r_prime)
+    psf_airy_disk = (2 * scipy.special.j1(r_prime) / r_prime) ** 2
+    psf_airy_disk /= np.sum(psf_airy_disk)
+    return psf_airy_disk
+
+
+def abs_and_remove_padding(c, pad_width):
+    return np.abs(c[pad_width:-pad_width, pad_width:-pad_width]) if pad_width > 0 else np.abs(c)
+
+
+def rebin(arr, new_shape):
+    shape = (new_shape[0], arr.shape[0] // new_shape[0], new_shape[1], arr.shape[1] // new_shape[1])
+    return arr.reshape(shape).mean(-1).mean(1)
+
+
+def slanted_edge_blurred_with_diffraction_only(shape, pixel_pitch, f_number, wavelength,
+                                               pad_width=10, oversampling=10):
+    import slanted_edge_target
+
+    roi_height, roi_width = (np.array(shape) + 2 * pad_width) * oversampling
+    im_roi = slanted_edge_target.make_ideal_slanted_edge((roi_height, roi_width))
+
+    grid_spacing = pixel_pitch / oversampling  # pixel_pitch is given in m
+
+    def calc_psf_shape(lam_diffr, f_number, grid_spacing, n=3):
+        # Calculate a psf_kernel size suitable for the psf blur size and pixel pitch
+        sz = int(n * lam_diffr * f_number * 2.44 / grid_spacing)  # set as n x first minimum dia.
+        return sz, sz
+
+    psf_shape = calc_psf_shape(wavelength, f_number, grid_spacing)
+    psf_diff = airy_disk(wavelength, f_number, grid_spacing, psf_shape)
+
+    blurred_im_roi = scipy.signal.convolve2d(im_roi, psf_diff, mode='same')
+
+    blurred_im_roi = rebin(blurred_im_roi, (np.array(blurred_im_roi.shape) / oversampling).astype(int))
+    blurred_im_roi = abs_and_remove_padding(blurred_im_roi, pad_width)
+
+    # try with Fourier transforms to speed up things
+    ft_psf = np.fft.fftshift(np.fft.fft2(airy_disk(wavelength, f_number, grid_spacing, im_roi.shape)))
+    ft_im_roi = np.fft.fftshift(np.fft.fft2(im_roi))
+    blurred_im_roi_from_ft = np.abs(np.fft.fftshift(np.fft.ifft2(ft_psf * ft_im_roi)))
+    blurred_im_roi_from_ft = rebin(blurred_im_roi_from_ft,
+                                   (np.array(blurred_im_roi_from_ft.shape) / oversampling).astype(int))
+    blurred_im_roi_from_ft = abs_and_remove_padding(blurred_im_roi_from_ft, pad_width)
+
+    return blurred_im_roi, blurred_im_roi_from_ft
+
+
+def extrap_mtf(input_lens_mtf, fit_begin=[60, 70], fit_end=[100, 90], mtf_fit_limit=[0.40],
+               mtf_tail_lvl=0.05, extend_to_fit=True):
+    """
+    The system / sensor MTF curve is unreliable at high spatial frequency, where the sensor MTF is small.
+    Therefore, we fit the lens MTF curve in a frequency interval and extrapolate down to zero MTF beyond that
+    interval with a soft tail. If needed, more points added to fit the whole MTF curve down to ~zero MTF.
+
+    :param input_lens_mtf: numpy array with spatial frequencies in cy/mm in the first row,
+           and MTF (0.0-1.0) values in the second row, which are obtained by dividing the system MTF by
+           the image sensor MTF (e.g. a sinc function)
+    :param fit_begin: list of two alternative start points for the fit interval (cy/mm)
+    :param fit_end: list of two alternative end points for the fit interval (cy/mm)
+    :param mtf_fit_limit: list of one MTF level at which we switch from trying to use the first fit interval to the second
+    :param mtf_tail_lvl: MTF beneath which the soft tail starts
+    :param extend_to_fit: Add more points to fit the whole MTF curve (if necessary)
+    :return:    2-d numpy array with spatial frequencies and lens MTF,
+                [fit range start used, fit range end used]
+    """
+
+    f, mtf = input_lens_mtf[:, 0], input_lens_mtf[:, 1]
+    mtf_ = scipy.interpolate.interp1d(f, mtf)
+
+    k = 1 if mtf_(fit_end[0]) < mtf_fit_limit[0] else 0
+    i = np.argwhere((fit_begin[k] <= f) & (f <= fit_end[k])).squeeze()
+    slope, offset = np.polyfit(f[i].squeeze(), mtf[i].squeeze(), 1)
+
+    x = copy.copy(f)
+    run_look = True
+    while run_look:
+        y = offset + slope * x
+        i0 = i[0]
+        y[0:i0] = mtf[0:i0].squeeze()
+        y[i0] = np.mean([mtf[i0], y[i0]])
+
+        if any(y < mtf_tail_lvl):
+            j0 = np.argwhere(y < mtf_tail_lvl)[0]
+        else:
+            j0 = len(y) - 1
+
+        if extend_to_fit:
+            if j0 < (len(y) - 10):
+                run_look = False
+            else:
+                x_ext = x[1:11] - x[0] + x[-1]
+                x = np.append(x, x_ext)
+        else:
+            run_look = False
+
+    j = np.arange(j0, len(y))
+    y[j] = y[j0] * (y[j0] / y[j0 - 1]) ** (j - j0)
+    return np.column_stack([x, y]), [fit_begin[k], fit_end[k]]
+
+
+def plot_image_and_crop_roi(fig, ax1, im, xc, yc, roi_height=80, roi_width=80):
+    import selection_rectangles
+    ax1.imshow(im, cmap='gray')
+    r = selection_rectangles.RectXY(fig, ax1)
+    fig.canvas.mpl_connect('motion_notify_event', r.move)
+    fig.canvas.mpl_connect('button_press_event', r.fix_or_release)
+    fig.canvas.mpl_connect('key_release_event', r.keypress)
+    roi_data = [{'x_center': xc, 'y_center': yc, 'height': roi_height, 'width': roi_width}]
+    r.populate_roi_data(roi_data)
+    # (selection_rectangles supports using multiple ROIs on the same image, but here we only use one ROI per image)
+    im_roi = selection_rectangles.crop(im, roi_data)[0]
+    return im_roi
+
+
+class MTFplotter:
+    def __init__(self, pixel_pitch, f_number=2.8, lam_diffr=550e-9, x_lims=[0, 120],
+                 fit_begin=[60, 70], fit_end=[100, 90], mtf_fit_limit=[0.40], mtf_tail_lvl=0.05):
+        self.pixel_pitch = pixel_pitch
+        self.f_number = f_number  # f-number for which we calculate diffraction
+        self.lam_diffr = lam_diffr  # wavelength for which we calculate diffraction
+        self.x_lims = x_lims  # plot limits for spatial frequency (cy/mm)
+        self.fit_begin = fit_begin  # Two alternative start points for the fit interval
+        self.fit_end = fit_end  # Two alternative end points for the fit interval
+        self.mtf_fit_limit = mtf_fit_limit  # Where we switch from trying to use the first fit interval to the second
+        self.mtf_tail_lvl = mtf_tail_lvl
+
+    def calc_and_plot_mtf(self, ax, im_roi, x_lims=None):
+        import utils
+        import SFR
+        # Calculate MTF from the ROI with the slanted edge
+        sfr_lin = SFR.SFR(quadratic_fit=False)  # force fitting to a straight edge, as in the ISO 12233 standard
+        mtf_lin, status = sfr_lin.calc_sfr(im_roi)
+        sfr = SFR.SFR()  # allow fitting to a 2nd order polynomial edge shape
+        mtf, status = sfr.calc_sfr(im_roi)
+
+        f0 = 1000 / self.pixel_pitch
+        mtf_lin[:, 0] *= f0
+        mtf[:, 0] *= f0
+
+        mtf_system = mtf
+        # ideal sensor MTF: 100% fill factor, no crosstalk
+        mtf_sinc = np.column_stack(
+            [mtf_system[:, 0], np.abs(np.sinc(mtf_system[:, 0] / f0))])  # TODO: add support for fill factor
+        mtf_lens_raw = np.column_stack([mtf_system[:, 0], mtf_system[:, 1] / (mtf_sinc[:, 1] + 1e-8)])
+
+        # The system / sensor MTF curve is unreliable at high spatial frequency, where the sensor MTF is small.
+        # Therefore, we fit the lens MTF curve in a frequency interval and extrapolate down to zero MTF beyond that
+        # interval with a soft tail. If needed, more points added to fit the whole MTF curve down to ~zero MTF.
+
+        mtf_lens, fit_range_used = \
+            utils.extrap_mtf(mtf_lens_raw, fit_begin=self.fit_begin, fit_end=self.fit_end,
+                             mtf_fit_limit=self.mtf_fit_limit, mtf_tail_lvl=self.mtf_tail_lvl, extend_to_fit=True)
+
+        # Diffraction limit MTF for reference (valid at wavelength == lam_diffr)
+        mtf_diff = np.column_stack([mtf_lens[:, 0],
+                                    utils.mtf_diffraction_limit(self.f_number, self.lam_diffr, mtf_lens[:, 0])])
+
+        ax.plot(mtf_lin[:, 0], mtf_lin[:, 1], '.:', color='C0', label="system MTF (lin. edge fit)")
+        ax.plot(mtf_system[:, 0], mtf_system[:, 1], '.-', color='C1', label="system MTF")
+        ax.plot(mtf_sinc[:, 0], mtf_sinc[:, 1], 'k-', label="ideal sensor MTF")
+        ax.plot(mtf_lens_raw[:, 0], mtf_lens_raw[:, 1], '--', color='C2', label="system MTF / sensor MTF")
+        ax.plot(mtf_lens[:, 0], mtf_lens[:, 1], '-.', color='C3',
+                label=f"lens MTF, fitted btw {fit_range_used[0]:.0f} and {fit_range_used[1]:.0f} cy/mm")
+        ax.plot(mtf_diff[:, 0], mtf_diff[:, 1], ':k',
+                label=f'diffraction limit for {self.lam_diffr / 1e-9:.0f} nm, f/{self.f_number:.1f}')
+
+        ax.set_xlim(*(x_lims if x_lims else self.x_lims))
+        ax.set_ylim(0, 1.2)
+        ax.grid()
+        ax.set_ylabel('MTF')
+        ax.set_xlabel('Spatial frequency (cy/mm)')
+        ax.legend(loc='best')
+        return mtf_system, mtf_lens, status
+
 
 def read_8bit(path):
     """
